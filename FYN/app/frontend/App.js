55c06264f7612093e6734dde24dddb3824f8d9f8
zyadelidrissi <77046942+zyadelidrissi@users.noreply.github.com>
2023-10-19 16:49:15 +0200

    [ADD] developpement .env variables

--- FYN/app/frontend/App.js
+++ FYN/app/frontend/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { API_TOKEN } from '@env';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start  on your app!</Text>
+      <Text>URL: {API_TOKEN}</Text>
       <StatusBar style="auto" />
     </View>
   );
