d02b39eeeb455aeb0d829cd072879d62e5efd2f4
Karim Louail <karim.louail@epitech.eu>
2023-11-10 14:06:42 +0100

    add style variables

---
+++ FYN/app/frontend/src/styles/styles.ts
@@ -0,0 +1,10 @@
+// styles.ts
+export const fynTheme = {
+  primaryColor: '#15A370',
+  secondaryColor: '#ffffffe3',
+  textPrimaryColor: '#ffffffe3',
+  textSecondaryColor: '0F704C',
+  normalFontSize: 36,
+
+};
+
